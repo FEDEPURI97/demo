4f88cd7d6f8d9f9a9876a8db64188010bb0a6246
puric <puricellafederico@gmail.com>
2025-12-25 17:36:04 +0100

    init

---
+++ asset/src/main/java/com/asset/asset/controller/ControllerAsset.java
@@ -0,0 +1,25 @@
+package com.asset.asset.controller;
+
+public class ControllerAsset {
+    /*
+    2️⃣ Asset Service
+
+    Gestisce:
+
+    laptop
+
+    smartphone
+
+    badge
+
+    licenze software
+
+    Responsabilità
+
+    inventario asset
+
+    assegnazione asset a dipendente
+
+    storico assegnazioni
+     */
+}
---
+++ employee/src/main/java/com/employee/employee/controller/ControllerEmploye.java
@@ -0,0 +1,71 @@
+package com.employee.employee.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("employees")
+public class ControllerEmploye {
+
+
+    @GetMapping
+    public void getEmployees(){
+        //Lista tutti i dipendenti
+    }
+    @GetMapping("/{id}")
+    public void getEmploye(Integer id){
+        //Recupera dettaglio dipendente
+    }
+    @PostMapping
+    public void createEmploye(Integer id){
+        //Crea un nuovo dipendente
+    }
+    @PutMapping("/{id}")
+    public void updateEmployees(Integer id){
+        //Aggiorna dati dipendente
+    }
+    @DeleteMapping("/{id}")
+    public void deleteEmployees(Integer id){
+        //Disattiva/elimina dipendente
+    }
+    @GetMapping("/{role}")
+    public void getEmployeByRole(String role){
+        //Filtra dipendenti per ruolo
+    }
+    @GetMapping("/{department}")
+    public void getEmploye(String department){
+        //Filtra dipendenti per reparto
+    }
+    /*
+
+
+    🧩 Microservizi principali (core)
+
+
+
+3️⃣ Project Service
+
+Gestisce:
+
+progetti aziendali
+
+date
+
+budget
+
+stato
+
+4️⃣ Allocation Service
+
+Gestisce:
+
+allocazione dipendenti → progetti
+
+percentuale (es. 50%)
+
+periodo
+
+     */
+
+}

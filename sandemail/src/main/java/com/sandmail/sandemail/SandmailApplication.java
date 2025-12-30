ec556cc6f358d253dfd596724abeaca6a704ab6c
puric <puricellafederico@gmail.com>
2025-12-30 17:07:56 +0100

    finish project test init sand email

---
+++ sandemail/src/main/java/com/sandmail/sandemail/SandmailApplication.java
@@ -0,0 +1,13 @@
+package com.sandmail.sandemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandmailApplication.class, args);
+	}
+
+}
